03168a66686e81a7c71dc7d6bba3fb149ea4c29f
mrStefanJ <stefanjoncic92@gmail.com>
2025-02-07 11:58:42 +0100

    add style in Login and Register page

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -2,12 +2,23 @@ import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../AuthProvider";
 import { Link, useNavigate } from "react-router";
 import { User } from "../types/User";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
   const { user, login } = useContext(AuthContext);
-  const [formData, setFormData] = useState<User>({ id:"" ,email: "", password: "" });
+  const [formData, setFormData] = useState<User>({
+    id: "",
+    email: "",
+    password: "",
+  });
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
+  const [showPassword, setShowPaassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPaassword((prev) => !prev);
+  };
 
   useEffect(() => {
     if (user) {
@@ -29,38 +40,59 @@ const Login = () => {
     } else {
       setMessage(result.message);
     }
-
   };
 
   return (
-    <div className="flex font-sans flex-col items-center mx-auto h-screen bg-gray-700">
-    <form onSubmit={handleSubmit} className="space-y-4 mt-40">
-      <h2>Login</h2>
-      <div className="flex flex-col space-y-6">
-      <input
-        type="email"
-        name="email"
-        placeholder="Enter your email"
-        value={formData.email}
-        onChange={handleChange}
-        required
-      />
-
-      <input
-        type="password"
-        name="password"
-        placeholder="Enter your password"
-        value={formData.password}
-        onChange={handleChange}
-        required
-      />
-      </div>
-      <p>
-        Don't have an account? <Link to="/register">Register here</Link>
-      </p>
-      <button type="submit">Login</button>
-      {message && <p className="text-red-700">{message}</p>}
-    </form>
+    <div className="flex font-sans flex-col items-center mx-auto h-screen bg-[#09112b]">
+      <form onSubmit={handleSubmit} className="space-y-4 mt-40 text-center">
+        <h1 className="text-white size-10 text-3xl w-full">Login</h1>
+        <div className="flex flex-col space-y-6">
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            className="shadow-lg shadow-yellow-700 rounded-full min-w-[80px] max-w-full px-3 py-2 focus:border-yellow-700 focus:outline-yellow-700"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Enter your password"
+              className="shadow-lg shadow-yellow-700 rounded-full w-full border p-2 focus:border-yellow-700 focus:outline-yellow-700"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute right-2 top-2 focus:outline-none"
+            >
+              {showPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon icon={faEye} />
+              )}
+            </button>
+          </div>
+        </div>
+        <p className="text-white">
+          Don't have an account?{" "}
+          <Link to="/register" className="hover:text-yellow-700">
+            Register here
+          </Link>
+        </p>
+        <button
+          type="submit"
+          className="bg-yellow-700 text-white px-4 py-2 rounded-full shadow-md hover:bg-yellow-800 transition"
+        >
+          Login
+        </button>
+        {message && <p className="text-red-700">{message}</p>}
+      </form>
     </div>
   );
 };
--- src/auth/Register.tsx
+++ src/auth/Register.tsx
@@ -2,6 +2,8 @@ import { ChangeEvent, FormEvent, useContext, useState, useId } from "react";
 import { AuthContext } from "../AuthProvider";
 import { Link } from "react-router";
 import { User } from "../types/User";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Register = () => {
   const { register } = useContext(AuthContext);
@@ -16,6 +18,11 @@ const Register = () => {
     image: "",
   });
   const [message, setMessage] = useState("");
+  const [showPassword, setShowPaassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPaassword((prev) => !prev);
+  };
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -25,12 +32,12 @@ const Register = () => {
   };
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file= e.target.files?.[0];
-    if(file) {
+    const file = e.target.files?.[0];
+    if (file) {
       const reader = new FileReader();
       reader.onload = () => {
         if (reader.result) {
-          setFormData({...formData, image: reader.result.toString()});
+          setFormData({ ...formData, image: reader.result.toString() });
         }
       };
       reader.readAsDataURL(file);
@@ -44,14 +51,15 @@ const Register = () => {
   };
 
   return (
-    <div className="flex font-sans flex-col items-center mx-auto h-screen bg-gray-700">
-      <form onSubmit={handleSubmit} className="space-y-4 mt-40">
-        <h2>Register</h2>
+    <div className="flex font-sans flex-col items-center mx-auto h-screen bg-[#09112b]">
+      <form onSubmit={handleSubmit} className="space-y-4 mt-40 text-center">
+        <h1 className="text-white size-10 text-3xl w-full">Register</h1>
         <div className="flex flex-col space-y-6">
           <input
             type="text"
             name="firstName"
             placeholder="Enter your first name"
+            className="shadow-lg shadow-yellow-700 rounded-full min-w-[80px] max-w-full px-3 py-2 focus:border-yellow-700 focus:outline-yellow-700"
             value={formData.firstName}
             onChange={handleChange}
             required
@@ -60,6 +68,7 @@ const Register = () => {
             type="text"
             name="lastName"
             placeholder="Enter your last name"
+            className="shadow-lg shadow-yellow-700 rounded-full min-w-[80px] max-w-full px-3 py-2 focus:border-yellow-700 focus:outline-yellow-700"
             value={formData.lastName}
             onChange={handleChange}
             required
@@ -68,28 +77,45 @@ const Register = () => {
             type="email"
             name="email"
             placeholder="Enter your email"
+            className="shadow-lg shadow-yellow-700 rounded-full min-w-[80px] max-w-full px-3 py-2 focus:border-yellow-700 focus:outline-yellow-700"
             value={formData.email}
             onChange={handleChange}
             required
           />
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Enter your password"
+              className="shadow-lg shadow-yellow-700 rounded-full w-full border p-2 focus:border-yellow-700 focus:outline-yellow-700"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute right-2 top-2 focus:outline-none"
+            >
+              {showPassword ? (
+                <FontAwesomeIcon icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon icon={faEye} />
+              )}
+            </button>
+          </div>
           <input
             type="date"
             name="birthDate"
             placeholder="Enter your birthday"
+            className="cursor-pointer shadow-lg shadow-yellow-700 rounded-full min-w-[80px] max-w-full px-3 py-2 focus:border-yellow-700 focus:outline-yellow-700"
             value={formData.birthDate}
             onChange={handleChange}
             required
           />
           <select
             name="gender"
+            className="cursor-pointer shadow-lg shadow-yellow-700 rounded-full min-w-[80px] max-w-full px-3 py-2 focus:border-yellow-700 focus:outline-yellow-700"
             value={formData.gender}
             onChange={handleChange}
             required
@@ -97,25 +123,48 @@ const Register = () => {
             <option value="" disabled>
               Select your genre
             </option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
+            <option
+              value="male"
+              className="px-3 py-2 cursor-pointer text-yellow-700 hover:bg-yellow-700"
+            >
+              Male
+            </option>
+            <option
+              value="female"
+              className="px-3 py-2 cursor-pointer text-yellow-700 hover:bg-yellow-700"
+            >
+              Female
+            </option>
           </select>
           <input
             type="file"
             accept="image/*"
+            className="hidden"
             onChange={handleImageChange}
             required
           />
+          <label
+            htmlFor="file-upload"
+            className="cursor-pointer bg-yellow-700 text-white px-4 py-2 rounded-full shadow-md hover:bg-yellow-800 transition"
+          >
+            Upload Image
+          </label>
         </div>
-        <p>
-          Already have an account? <Link to="/login">Login here</Link>
+        <p className="text-white">
+          Already have an account?{" "}
+          <Link to="/login" className="hover:text-yellow-700">
+            Login here
+          </Link>
         </p>
-        <button type="submit">Register</button>
+        <button
+          type="submit"
+          className="bg-yellow-700 text-white px-4 py-2 rounded-full shadow-md hover:bg-yellow-800 transition"
+        >
+          Register
+        </button>
         {message && <p className="bg-red-700">{message}</p>}
       </form>
-      <div className="table">
-        
-      </div>
+      <div className="table"></div>
     </div>
   );
 };
