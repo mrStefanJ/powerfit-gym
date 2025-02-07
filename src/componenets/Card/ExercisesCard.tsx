2a24cba1e905773e5fcec3e655798c6d90a5e78c
mrStefanJ <stefanjoncic92@gmail.com>
2025-02-07 14:56:47 +0100

    remove

--- src/componenets/Card/ExercisesCard.tsx
+++ src/componenets/Card/ExercisesCard.tsx
@@ -1,29 +1,79 @@
-import { Exercise } from "../../types/Exercis";
+import { useState } from "react";
+import { Category, Workout } from "../../types/Exercis";
 import { useDraggable } from "@dnd-kit/core";
 
 type ExercisCardProps = {
-  task: Exercise;
+  exercise: Workout;
+  columnId: string;
+  setExercises: React.Dispatch<React.SetStateAction<Category[]>>;
 };
 
-const ExercisesCard = ({ task }: ExercisCardProps) => {
+const ExercisesCard = ({ exercise, columnId, setExercises }: ExercisCardProps) => {
+  const [inputValue, setInputValue] = useState<number>(5);
+
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: task.id,
+    id: exercise.id.toString(),
   });
+
+  // useEffect(() => {
+  //   setInputValue(exercise.sets ?? 5);
+  // }, [exercise.sets]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    const newValue = value >= 0 ? value : 0;
+    
+    setInputValue(newValue);
+  
+    setExercises((prev) => {
+      if (!prev || !Array.isArray(prev)) return [];
+  
+      return prev.map((category) => ({
+        ...category,
+        exercises: {
+          ...category.exercises,
+          workout: category.exercises.workout.map((ex) =>
+            ex.id === exercise.id ? { ...ex, sets: newValue } : ex
+          ),
+        },
+      }));
+    });
+  };  
 
   const style = transform
     ? {
         transform: `translate(${transform.x}px, ${transform.y}px)`,
       }
     : undefined;
+    
   return (
     <div
       ref={setNodeRef}
       {...listeners}
       {...attributes}
-      className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md"
+      className="cursor-grab rounded-lg bg-neutral-700 p-6 w-2xs shadow-sm hover:shadow-md"
       style={style}
     >
-      <h3 className="font-medium text-neutral-100">{task.title}</h3>
+      <h3 className="w-[60px] font-medium text-neutral-100">{exercise.title}</h3>
+
+      {columnId === "IN_PROGRESS" && (
+        <input
+          type="number"
+          min="0"
+          value={inputValue}
+          onChange={handleInputChange}
+          className="w-[80px] p-2 rounded"
+        />
+      )}
+
+      {columnId === "DONE" && (
+        <input
+          type="number"
+          value={inputValue}
+          className="w-[80px] p-2 rounded"
+          disabled
+        />
+      )}
     </div>
   );
 };
